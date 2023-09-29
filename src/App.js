73e0a86e21c1bc70e71279e7fcfac2e8de397621
Sahil Singh <singhsahil140404@gmail.com>
2023-09-29 15:22:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+import './style.css';
+import { toPng } from 'html-to-image';
+import download from 'downloadjs';
+import cakes from './images/cakes.png';
 
-function App() {
+
+export default function App() {
+  const [text, setText] = React.useState("")
+
+  function handleText(event) {
+    const newText = event.target.value
+    setText(newText)
+
+  }
+
+  const node = document.getElementById('image-download')
+  function downloadImage() {
+    toPng(node)
+      .then(dataURL => {
+        download(dataURL, "custom-image.png")
+      })
+      .catch(() => console.log("Error"))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-outer">
+      <div className='container'>
+        <h1 className='name'>Enter the name of person whom you want to wish Happy Birthday!!!🎉🥳🎈🎈🎈</h1>
+        <input type="text" placeholder='Type text here' className='input' onChange={handleText} value={text} />
+
+        <div className='image-wrapper' id='image-download'>
+          <img src={cakes} width={"500px"} height={"500px"} alt="Cake Image" className='image' />
+          <div className='image-text-div'>
+            <h1 className="image-text">{text}</h1>
+          </div>
+        </div>
+        <button className='download-button' onClick={downloadImage}>Download Image</button>
+
+      </div>
     </div>
-  );
+
+  )
+
 }
 
-export default App;
+// export default App;
